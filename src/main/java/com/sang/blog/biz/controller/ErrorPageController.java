10ed78d6c072b6b235e7616dd97017c30d287f53
sang666 <2838441929@qq.com>
2020-08-12 13:02:58 +0800

    修复了点小bug

--- src/main/java/com/sang/blog/biz/controller/ErrorPageController.java
+++ src/main/java/com/sang/blog/biz/controller/ErrorPageController.java
@@ -3,6 +3,7 @@ package com.sang.blog.biz.controller;
 
 import com.sang.blog.commom.result.Result;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 
 public class ErrorPageController {
-    @GetMapping("/404")
+    /*@GetMapping("/404")
     public Result page404(){
         return Result.ERROR_404();
 
@@ -34,6 +35,26 @@ public class ErrorPageController {
     public Result page505(){
         return Result.ERROR_505();
 
+    }*/
+
+    @RequestMapping("/404")
+    public Result page404() {
+        return Result.ERROR_404();
+    }
+
+    @RequestMapping("/403")
+    public Result page403() {
+        return Result.ERROR_403();
+    }
+
+    @RequestMapping("/504")
+    public Result page504() {
+        return Result.ERROR_504();
+    }
+
+    @RequestMapping("/505")
+    public Result page505() {
+        return Result.ERROR_505();
     }
 
 
